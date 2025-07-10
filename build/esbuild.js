0b843029374912c659ecd1a4c8eb8e5c7f3f8e1d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-07-10 13:04:38 -0500

    added build

--- build/esbuild.js
+++ build/esbuild.js
@@ -5,6 +5,7 @@ await esbuild.initialize({
   wasmURL: 'https://unpkg.com/esbuild-wasm/esbuild.wasm',
 })
 
-const result = await esbuild.transform(code, options)
-
-return result.code
+export const build = async (code, options) => {
+  const result = await esbuild.transform(code, options)
+  return result.code
+}

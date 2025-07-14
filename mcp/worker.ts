1b27c9e2e91e4ef5659d4fabfd93db146d7b9967
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-07-14 16:47:52 -0500

    added mcp support

---
+++ mcp/worker.ts
@@ -0,0 +1,40 @@
+import { env, WorkerEntrypoint } from 'cloudflare:workers'
+import { experimental_createMCPClient } from 'ai'
+import { createOpenAI } from '@ai-sdk/openai'
+
+// const openai = createOpenAI({
+//   apiKey: env.OPENAI_API_KEY,
+// })
+
+const servers = {
+  context7: 'https://context7.liam.sh/sse',  // 'https://context7.liam.sh/mcp',
+  deepwiki: 'https://mcp.deepwiki.com/sse',
+  slack: 'https://mcp.slack.com/sse',
+  github: '',
+  linear: '',
+  stripe: '',
+  cloudflare: '',
+} as any
+
+
+export default class extends WorkerEntrypoint {
+
+  async tools(server: any = 'deepwiki') {
+
+    const client = await experimental_createMCPClient({
+      transport: {
+        type: 'sse',
+        url: servers[server] || 'https://' + server,
+      },
+    })
+
+    const tools = await client.tools()
+    console.log(tools)
+    return tools
+  }
+
+  async fetch(request: Request) {
+    const { pathname } = new URL(request.url)
+    return Response.json(await this.tools(pathname.slice(1) as keyof typeof servers & string))
+  }
+}

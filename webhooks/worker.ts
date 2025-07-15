417c83f2598a19e4a47a28cdf131ee487f2b5d3d
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-07-15 13:00:21 -0500

    added webhooks queue

---
+++ webhooks/worker.ts
@@ -0,0 +1,51 @@
+import { env, WorkerEntrypoint } from 'cloudflare:workers'
+
+export default class extends WorkerEntrypoint {
+
+
+  // save(data: any) {
+  //   const { pathname } = new URL(request.url)
+  //   const name = pathname.slice(1)
+  //   const worker = await this.getWorker(name).then(res => res.json())
+  //   const deployTime = Date.now() - start
+  //   return Response.json({ name, worker, deployTime })
+  // }
+
+  // send(data: any) {
+  //   const { pathname } = new URL(request.url)
+  //   const name = pathname.slice(1)
+  //   const worker = await this.getWorker(name).then(res => res.json())
+  //   const deployTime = Date.now() - start
+  //   return Response.json({ name, worker, deployTime })
+  // }
+
+  async fetch(request: Request) {
+    try {
+      const { origin, hostname, pathname, searchParams } = new URL(request.url)
+      const headers = Object.fromEntries(request.headers)
+      const query = Object.fromEntries(searchParams)
+      let body: any
+      try {
+        body = await request.text()
+        body = JSON.parse(body)
+      }
+      catch (error) { }
+
+      const event = {
+        $type: 'Webhook.Received',
+        origin,
+        hostname,
+        pathname,
+        query,
+        headers,
+        body,
+      }
+      const result = await env.pipeline.send([event])
+      return Response.json({ success: true, result })
+    }
+    catch (error) {
+      return Response.json({ error: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 })
+    }
+  }
+
+}

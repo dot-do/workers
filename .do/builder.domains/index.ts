4dbd13dcaafc50cd5691a9348d4f8eeb7f2805aa
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-07-22 10:48:00 -0500

    questions identifies

--- .do/builder.domains/index.ts
+++ .do/builder.domains/index.ts
@@ -1,34 +1,55 @@
 // # Open Questions
+// - Do we allow arbitray args in functions, or require a single object like React Props?
 // - How do we pass context/user/tenant/subscription information into the function?
 // - How do we handle authentication/authorization?
 // - How do we enforce/limit free/paid tiers (like say 3 free subdomains, etc)?
+// - How do we expose functions via MCP?
+// - How do we expose functions via REST?
 
-export const searchSubdomains = (subdomain: string) => {
+
+type Context = {
+  user: {
+    id: string
+    email: string
+    name: string
+  }
+  tenant: {
+    id: string
+    name: string
+  }
+  subscription: {
+    id: string
+    plan: string
+    status: string
+  }
+}
+
+export const searchSubdomains = (props: { subdomain: string }, context: Context) => {
   // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
   // const results = db.domains.find`https://builder.domains/${subdomain}.*`
 }
 
-export const claimSubdomain = (subdomain: string) => {
+export const claimSubdomain = (props: { subdomain: string }, context: Context) => {
   // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
   // const results = db.domains.find`https://builder.domains/${subdomain}.*`
 }
 
-export const releaseSubdomain = (subdomain: string) => {
+export const releaseSubdomain = (props: { subdomain: string }, context: Context) => {
   // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
   // const results = db.domains.find`https://builder.domains/${subdomain}.*`
 }
 
-export const listSubdomains = (context: any) => {
+export const listSubdomains = (props: { limit?: number }, context: Context) => {
   // TODO: figure out how to pass current user/tenant context in the function?
   // const results = db.domains.find({ user: context.user.id })
 }
 
-export const getSubdomainConfig = (subdomain: string) => {
+export const getSubdomainConfig = (props: { subdomain: string }, context: Context) => {
   // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
   // const results = db.domains.find`https://builder.domains/${subdomain}.*`
 }
 
-export const setSubdomainConfig = (subdomain: string, config: any) => {
+export const setSubdomainConfig = (props: { subdomain: string, config: any }, context: Context) => {
   // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
   // const results = db.domains.find`https://builder.domains/${subdomain}.*`
 }

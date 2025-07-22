16f1867663f1e0d101a041fa74bba08d0aa23496
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-07-22 10:36:28 -0500

    creating initial builder.domains functions

---
+++ .do/builder.domains/index.ts
@@ -0,0 +1,34 @@
+// # Open Questions
+// - How do we pass context/user/tenant information into the function?
+// - How do we handle authentication/authorization?
+
+
+export const searchSubdomains = (subdomain: string) => {
+  // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
+  // const results = db.domains.find`https://builder.domains/${subdomain}.*`
+}
+
+export const claimSubdomain = (subdomain: string) => {
+  // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
+  // const results = db.domains.find`https://builder.domains/${subdomain}.*`
+}
+
+export const releaseSubdomain = (subdomain: string) => {
+  // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
+  // const results = db.domains.find`https://builder.domains/${subdomain}.*`
+}
+
+export const listSubdomains = (context: any) => {
+  // TODO: figure out how to pass current user/tenant context in the function?
+  // const results = db.domains.find({ user: context.user.id })
+}
+
+export const getSubdomainConfig = (subdomain: string) => {
+  // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
+  // const results = db.domains.find`https://builder.domains/${subdomain}.*`
+}
+
+export const setSubdomainConfig = (subdomain: string, config: any) => {
+  // TODO: figure out API/Syntax/implementation to search for lack of subdomain existence
+  // const results = db.domains.find`https://builder.domains/${subdomain}.*`
+}
